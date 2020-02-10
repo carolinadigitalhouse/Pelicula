2ffd35565b4074d4328cc6c3d87b36d1d3df05d9
eamaya2008 <43824734+eamaya2008@users.noreply.github.com>
2020-02-10 15:42:33 -0300

    Cambio entity y app.prop

--- src/main/java/com/dh/pelicula/entidades/Pelicula.java
+++ src/main/java/com/dh/pelicula/entidades/Pelicula.java
@@ -3,6 +3,7 @@ package com.dh.pelicula.entidades;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
@@ -12,7 +13,7 @@ public class Pelicula {
 
     @Id
     @Column(name = "id")
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String titulo;
